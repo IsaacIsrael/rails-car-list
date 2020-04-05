74caf1faf659a4a7936669569980fab57ad35052
isaac israel <email.iisrael@gmail.com>
2020-04-05 14:15:59 -0300

    Style the componnets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
 import "bootstrap";
+import 'select2/dist/css/select2.css';
+import "select2-bootstrap-theme/dist/select2-bootstrap.css";
+
+import { initSelect2 } from '../components/init_select2';
+
+initSelect2();

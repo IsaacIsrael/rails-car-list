bf2c0852fd22ccdd9d32152958d9b06ae3760d85
isaac israel <email.iisrael@gmail.com>
2020-04-12 20:06:16 -0300

    Create Controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    @colors = Color.all
+    @search = params[:search] || {}
+    @cars = Car.filter(@search).page(params[:page])
   end
 end
